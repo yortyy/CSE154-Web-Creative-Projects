43d8f087e088b8a9fefcaed9ac374c9d49164ae4
yortyy <lan30san@gmail.com>
2024-06-20 13:30:58 -0700

    added backgrounds, js for changing background image, centered/stylized page

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * index.js for CSE 154 creative project homepage. Sets the background using a button.
+ */
+
+
+"use strict";
+
+(function() {
+  const NUMBER_OF_BACKGROUNDS = 17;
+  let currentBackground = 4;
+  let pastBackground = 0;
+  let past2Background = 0;
+  let past3Background = 0;
+  let past4Background = 0;
+
+  window.addEventListener("load", init);
+
+  function init() {
+    qs("#bgchange").addEventListener("click", changeBG);
+  }
+
+  function changeBG() {
+    let rand = Math.ceil(Math.random() * NUMBER_OF_BACKGROUNDS);
+    while(rand == currentBackground || rand == pastBackground || rand == past2Background ||
+          rand == past3Background || rand == past4Background) {
+      rand = Math.ceil(Math.random() * NUMBER_OF_BACKGROUNDS);
+    }
+    past4Background = past3Background;
+    past3Background = past2Background;
+    past2Background = pastBackground;
+    pastBackground = currentBackground;
+    currentBackground = rand;
+    qs("body").style.backgroundImage = "url(imgs/backgrounds/" + rand + ".gif)";
+  }
+
+  /**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+})();
